50b5e635885c1e8f25bfbbc9df460efcae84fd89
Nikith <111171016+Nikith2@users.noreply.github.com>
2023-02-01 12:35:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let homescoreEl=document.getElementById("home-score")
+let sumscore=0
+function add1(){
+    sumscore+=1
+    homescoreEl.textContent=sumscore
+}
+function add2(){
+    sumscore+=2
+    homescoreEl.textContent=sumscore
+}
+function add3(){
+    sumscore+=3
+    homescoreEl.textContent=sumscore
+}
+let guestscoreEl=document.getElementById("guest-score")
+let guestscore=0
+function add11(){
+    guestscore+=1
+    guestscoreEl.textContent=guestscore
+}
+function add12(){
+    guestscore+=2
+    guestscoreEl.textContent=guestscore
+}
+function add13(){
+    guestscore+=3
+    guestscoreEl.textContent=guestscore
+}
